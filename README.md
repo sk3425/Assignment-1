dfae241a2d0a25ee672a0e6839a2fef8815f8969
sk3425 <sk3425@cornell.edu>
2025-02-05 18:00:11 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# Repository for my projects 
+This repository was built for Juan's class, spring 2025.
---
+++ map.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoic2szNDI1IiwiYSI6ImNtNmpmeTVnYzAwaTEyaXE4ZWZ4OG9yazIifQ.TsQiMmufb4nsdomDobl1nA';
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/sk3425/cm6o41tya01b701qmdpu9boh5',
+    zoom: 10,
+    center: [-74, 40.725]
+});
